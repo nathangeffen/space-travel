64eb746268a80fa70e280e524bda7cb2e9a81006
Nathan Geffen <nathangeffen@gmail.com>
2012-07-03 23:58:30 +0200

    Added some text.

--- spacefooter.php
+++ spacefooter.php
@@ -2,6 +2,7 @@
    <p>Copyright (C) <a href="http://nathangeffen.webfactional.com">Nathan Geffen</a> 2012 under the 
    <a href="http://www.gnu.org/licenses/agpl.html
 ">GNU Affero General Public License</a>.<br />
-   This software is available <a href="">here</a>.
+   This software is available <a href="https://github.com/nathangeffen/space-travel">here</a>.<br />
+   There are probably bugs, bad ones. Please tell me about them at nathangeffen at quackdown dot info.
    </p>
 </div>
